089db7d50af41c7d7fa6a22a5a098397a2d54c74
Dasun Bandara <dasun8517@gmail.com>
2024-04-07 23:05:24 +0530

    Add chargers component & charge-row component

---
+++ src/app/view/charge-row/charge-row.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charge-row',
+  template: `
+    <p>
+      charge-row works!
+    </p>
+  `,
+  styleUrl: './charge-row.component.scss'
+})
+export class ChargeRowComponent {
+
+}
---
+++ src/app/view/chargers/chargers.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chargers',
+  template: `
+    <p>
+      chargers works!
+    </p>
+  `,
+  styleUrl: './chargers.component.scss'
+})
+export class ChargersComponent {
+
+}
